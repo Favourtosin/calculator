5a0a1c208bf5f0c57740558e143b70bfd0304280
Fakiya Favour <88689176+Favourtosin@users.noreply.github.com>
2022-06-23 02:06:28 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+addEventListener("load",  function () {
+   alert("welcome! You can make use of the calculator using the console. Thanks")
+} )
+
+addEventListener("load",  function () {
+    alert("using the calculator is quite simple. input the operator, open bracket notation and input two values. For example, add(2,2). ")
+ } )
+
+function add (num1,num2){
+return	num1+ num2; }
+
+function divide (num1,num2){
+return num1/num2;}
+
+function subtract(num1, num2){
+return num1-num2;}
+
+function multiply(num1, num2){
+return	num1 * num2;}
+
